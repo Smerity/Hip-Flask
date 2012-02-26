6a94bbd2feff1f5554a957cb820c1a0c3a9c2538
Stephen Merity <smerity@smerity.com>
2012-02-26 23:33:38 +1100

    Hip-Flask supports OpenID, an admin area, Bootstrap stylings and safe sessions via ItsDangerous

---
+++ admin.py
@@ -0,0 +1,16 @@
+import flask
+
+admin_console = flask.Blueprint("admin_console", __name__)
+
+@admin_console.before_request
+def restrict_to_admins():
+  # TODO: This should be secure for your web application
+  if not flask.g.user or "admin" not in flask.g.user:
+    content = "# Admin Console\n<i class='icon icon-ban-circle'></i> Access denied due to high level retina and fingerprint check!\n\n"
+    content += "<a href='%s'><i class='icon icon-fire'></i> Haxx0r t3h syst3m?</a>" % flask.url_for("admin_hacked")
+    return flask.render_template("md_template.html", content=content), 403
+
+@admin_console.route("/")
+def admin_index():
+  content = "# Admin Console\n<i class='icon icon-signal'></i> Super secret secure information!"
+  return flask.render_template("md_template.html", content=content)
---
+++ flask_extensions.py
@@ -0,0 +1,26 @@
+import flask
+from app import app
+
+import markdown
+import os
+
+# See http://flask.pocoo.org/snippets/40/ (static cache buster)
+## TODO: Caching should occur on the file time as otherwise os.stat is called every request
+@app.context_processor
+def _context_hash_url_for():
+  return dict(dated_url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+  if endpoint == "static":
+    filename = values.get("filename", None)
+    if filename:
+      file_path = os.path.join(app.root_path, endpoint, filename)
+      values["q"] = int(os.stat(file_path).st_mtime)
+    return flask.url_for(endpoint, **values)
+
+## Markdown filter and render
+
+md = markdown.Markdown()
+@app.template_filter("markdown")
+def render_markdown(content):
+  return flask.Markup(md.convert(content))
