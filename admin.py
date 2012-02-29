b42ab7f34cedcd4adf0cddb00dc85dcd954cbe2c
Stephen Merity <smerity@smerity.com>
2012-02-29 14:32:38 +1100

    admin.py: Mention the issue with performing auth checks on before_request

--- admin.py
+++ admin.py
@@ -5,6 +5,7 @@ admin_console = flask.Blueprint("admin_console", __name__)
 @admin_console.before_request
 def restrict_to_admins():
   # TODO: This should be secure for your web application
+  # NOTE: It may make sense to instead use a login_required decorator but before_request is safest as it cannot be forgotten
   if not flask.g.user or "admin" not in flask.g.user:
     content = "# Admin Console\n<i class='icon icon-ban-circle'></i> Access denied due to high level retina and fingerprint check!\n\n"
     content += "<a href='%s'><i class='icon icon-fire'></i> Haxx0r t3h syst3m?</a>" % flask.url_for("admin_hacked")
